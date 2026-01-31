24463c5a2fb89ca4f9e9151c88dcf83621030b87
Feyisetan Bidemi Salau <feyisetansalau@gmail.com>
2026-01-31 07:45:10 +0100

    Accept Inputs from the User
    Accept inputs from the user and print out the total number of characters entered.

--- core.py
+++ core.py
@@ -7,3 +7,11 @@ print("\nWELCOME TO THE PASSWORD GENERATOR PROGRAMME!")
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '', ']', '^', '_', '{', '|', '}', '~']
+
+#Accept inputs from the user.
+letter_characters = int(input("\nEnter the number of letters you want in your password: "))
+number_characters = int(input("Enter the number of numbers you want in your password: "))
+symbol_characters = int(input("Enter the number of symbols you want in your password: "))
+
+characters_total = letter_characters + number_characters + symbol_characters
+print(f"\nThe total number of characters you want is: '{characters_total}'  characters.")
